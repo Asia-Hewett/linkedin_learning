8aced340b9bde284aac5bc932a9a08dd0702f9ac
Asia-Hewett <asia.hewett0046@gmail.com>
2020-10-23 16:43:48 -0500

    Adding Ch2 Python

---
+++ learning_python/Ch2/helloworld.py
@@ -0,0 +1,21 @@
+#
+# Example file for HelloWorld
+
+# print("Hello world!")
+
+# name = input("What is your name? ")
+
+# name
+
+# print("Nice to meet you,", name)
+
+#
+# Example file for HelloWorld
+
+def main():
+    print("hello world!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
